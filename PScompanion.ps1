21fe5ebc2b93b31b3ce9b8ee7c1e036650d09a22
DakshiVenkat <100416641+DakshiVenkat@users.noreply.github.com>
2023-07-03 11:56:17 +0530

    Update PScompanion.ps1

--- PScompanion.ps1
+++ PScompanion.ps1
@@ -19,7 +19,7 @@ if($opt -eq 1)
 {
 write-host "Selected Option is Get-Service"
 $status_name = read-host -Prompt "Enter the Status Property Value[Stopped/Running]"
-$dis_name = read-host -Prompt "Enter the Displayname Property Value`n`t1.Starts with :: Keyword*`n`t2.Ends with :: *Keyword`n`t3.Contains :: *Keyword*`n`t4.Exact :: Keyword`n$("-"*40)`nEnter Input for DisplayName Property Value::"
+$dis_name = read-host -Prompt "Enter the Display name Property Value`n`t1.Starts with :: Keyword*`n`t2.Ends with :: *Keyword`n`t3.Contains :: *Keyword*`n`t4.Exact :: Keyword`n$("-"*40)`nEnter Input for DisplayName Property Value::"
 $r1 = get-service | Where-Object -FilterScript {$_.Status -eq $status_name -and $_.DisplayName -like $dis_name}
 $r1 | Select-Object -Property Name,StartType,Status,DisplayName |ft
 $total1 = ($r1).count
@@ -33,7 +33,7 @@ write-host "Selected Option is Get-Process" -f yellow
 [int32]$H_strvalue = read-host -Prompt "Enter the Handles Starting value[100 to 500]"
 [int]$H_endvalue = read-host -Prompt "Enter the Handles Ending value[2000 to 3000]"
 
-#Loading the Result in to the Variable(Assigmnment)
+#Loading the Result into the Variable(Assignment)
 $r2 = Get-Process | Where-Object -FilterScript {$_.handles -gt $H_strvalue -and $_.Handles -lt $H_endvalue}
 #Printing on the Screen
 $r2 = $r2 | Sort-Object -property Handles |ft
@@ -45,7 +45,7 @@ write-host "Result stored in a CSV file in D drive"
 if($opt -eq 3)
 {
 clear-host
-write-host "Selected Option is Get-Evetnlog`n$("*"*40)" -f yellow
+write-host "Selected Option is Get-Eventlog`n$("*"*40)" -f yellow
 
 
 "1 Application
@@ -59,7 +59,7 @@ write-host "Selected Option is Get-Evetnlog`n$("*"*40)" -f yellow
 
 #input1
 $lname = read-host -Prompt "`n`n`n`t`tEnter the Logname From the above list" #Application
-#$lname = read-host -Prompt "Enter the logname serial Number"
+#$lname = read-host -Prompt "Enter the logname Serial Number"
 
 "
 - Error
@@ -95,7 +95,7 @@ write-host "Result stored in a CSV file in D drive"
 if($opt -eq 4)
 {
 
-$dir_name = read-host -Prompt "Entery the Directory/Folder Size" #D:\PS
+$dir_name = read-host -Prompt "Enter the Directory/Folder path" #D:\PS
 
 if(test-path -Path $dir_name)
 {
